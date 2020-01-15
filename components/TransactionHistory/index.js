a47f6f18114549bad9e849c5e838c1275e41ee20
lolieatapple <lolieatapple@gmail.com>
2020-01-15 10:51:28 +0800

    transaction history ui finish.

--- components/TransactionHistory/index.js
+++ components/TransactionHistory/index.js
@@ -94,6 +94,53 @@ class TransactionHistory extends Component {
       amount: 100,
       type: 'UP',
       result: 'Done',
+    }, {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '03',
+      amount: 100,
+      type: 'UP',
+      result: 'Done',
+    }, 
+    {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '05',
+      amount: 100,
+      type: 'DOWN',
+      result: 'to be settled',
+    }, 
+    {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '02',
+      amount: 100,
+      type: 'DOWN',
+      result: 'Done',
+    }, 
+    {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '07',
+      amount: 100,
+      type: 'Return',
+      result: 'to be settled',
+    }, 
+    {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '-',
+      amount: 100.1234,
+      type: 'Fee distribution',
+      result: 'Done',
+    }, 
+    {
+      time: '2020-01-14 17:46:39',
+      address: '0x4cf0a877e906dead748a41ae7da8c220e4247d9e',
+      round: '03',
+      amount: 100,
+      type: 'UP',
+      result: 'Done',
     }, 
   ]
 
@@ -111,7 +158,7 @@ class TransactionHistory extends Component {
           <div className={style.subTitle}>Transaction History</div>
           <div className={style.rightText}>Total: 2000 WAN</div>
         </div>
-        <Table columns={this.columns} dataSource={this.dataSource}/>
+        <Table columns={this.columns} dataSource={this.dataSource} pagination={{ pageSize: 4 }}/>
       </div>
     );
   }
