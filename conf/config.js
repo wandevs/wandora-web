4c2662d4a4ab680ab9c25ae52c47ac980c4fe726
lolieatapple <lolieatapple@gmail.com>
2020-04-25 22:50:19 +0800

    update sc addr

--- conf/config.js
+++ conf/config.js
@@ -3,10 +3,10 @@ export const mainnetSCAddrWan2Btc = '0xdfad0145311acb8f0e0305aceef5d11a05df9aa0'
 export const testnetSCAddrWan2Btc = '0x6e1f4097ec38965256a17a9c8ed3ef38162647ad';//testnet 8 hours smart contract
 
 export const mainnetSCAddrBtc2Usd = '';//mainnet 8 hours smart contract
-export const testnetSCAddrBtc2Usd = '0x0bee9c6834cd60e65e25caf616572052583bb045';//testnet 8 hours smart contract
+export const testnetSCAddrBtc2Usd = '0xee46619fdc348831e2f0a592f22584dbffb0c85a';//testnet 8 hours smart contract
 
 export const mainnetSCAddrEtc2Usd = '';//mainnet 8 hours smart contract
-export const testnetSCAddrEtc2Usd = '0xaca01daa56bf2e9e68ca52a64cdd16c95d904a54';//testnet 8 hours smart contract
+export const testnetSCAddrEtc2Usd = '0x1faac972ef5148cf1343d790768b61a45da38f9b';//testnet 8 hours smart contract
 
 
 // change networkId to switch network
